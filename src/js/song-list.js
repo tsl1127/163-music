7fadbd0de83346b0049775e7a14126f11b7fd376
tsl <taosiliang1127tsl@163.com>
2018-03-03 16:49:55 +0800

    选中歌曲后在表单里展示歌曲

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -61,7 +61,20 @@
             $(this.view.el).on('click','li',(e)=>{
                 this.view.activeItem(e.currentTarget)
                 let songId=e.currentTarget.getAttribute('data-song-id')
-                window.eventHub.emit('select',{id:songId})
+                let data
+                let songs=this.model.data.songs
+                for(i=0;i<songs.length;i++){
+                    if(songs[i].id===songId){
+                        data=songs[i]     //从歌单中找到了选中的歌曲
+                        break
+                    }
+                }
+                // console.log('for 循环结束后的data')
+                // console.log(data)
+                //深拷贝
+                let string = JSON.stringify(data)
+                let object = JSON.parse(string)   //object就是新的内存
+                 window.eventHub.emit('select',object)
             })
         },
         bindEventHub(){
